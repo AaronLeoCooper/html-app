a5527052cea545a0952d7ceba4c2d85aff244b7e
Aaron Leo Cooper <aaronleocooper@gmail.com>
2019-02-09 14:13:33 +0800

    feat: Updated build & dev configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,9 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import serve from 'rollup-plugin-serve';
+
+import { name as projectName } from './package.json';
+
 const isProduction = process.env.NODE_ENV === 'production';
 
 console.log('isProduction', isProduction);
@@ -6,8 +12,25 @@ module.exports = {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/simple-renderer.browser.js',
-      format: 'umd'
+      file: `dist/${projectName}.browser.js`,
+      format: 'umd',
+      name: 'HTMLApp'
+    },
+    {
+      file: `dist/${projectName}.es.js`,
+      format: 'es'
     }
-  ]
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    !isProduction && serve({
+      open: true,
+      openPage: '/example.html',
+      contentBase: ['src', 'dist'],
+      port: 3000
+    })
+  ].filter(Boolean)
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-function getAllDomNodes(doc) {
-  return doc.querySelectorAll('[data-simpren]');
+class HTMLApp {
+  constructor(opts) {
+    this.root = this.getRootElement();
+
+    if (opts.onLoad) {
+      const nodes = this.getNodes();
+
+      opts.onLoad(nodes);
+    }
+  }
+
+  getRootElement() {
+    return document.querySelector('[data-htmlapp]');
+  }
+
+  getNodes() {
+    const nodes = this.root.querySelectorAll('[data-sa]');
+
+    return nodes.map(node => ({
+      setHtml: htmlStr => {
+        node.innerHTML = htmlStr;
+      }
+    }));
+  }
 }
 
-getAllDomNodes(document);
+export default HTMLApp;
