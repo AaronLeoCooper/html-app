f25130a86bfe35ddbbf61e54c845a781a0fa80e2
Aaron Leo Cooper <aaronleocooper@gmail.com>
2019-02-25 20:43:08 +0800

    feat: Added helper functions to element enhancement function

--- src/elements.js
+++ src/elements.js
@@ -2,10 +2,10 @@ import { CHILD_EL_ATTR } from './constants';
 
 /**
  * Returns an object wrapping the passed DOM element with helper functions.
- * @param element {Element}
- * @returns {{setInnerHtml: setHtml, el: Element}}
+ * @param el {Element} The DOM element being wrapped
+ * @returns {{el: Element, id: string}}
  */
-export function getEnhancedElement(element) {
+export function getEnhancedElement(el) {
   /**
    * Returns a function that invokes the passed callback before returning the whole
    * wrapper object to allow chaining methods.
@@ -18,10 +18,41 @@ export function getEnhancedElement(element) {
   };
 
   const wrapper = {
-    el: element,
-    id: element.getAttribute(CHILD_EL_ATTR),
+    el,
+    id: el.getAttribute(CHILD_EL_ATTR),
+
+    // Setters
     setInnerHtml: withWrapper(htmlStr => {
-      element.innerHTML = htmlStr;
+      el.innerHTML = htmlStr;
+    }),
+    setText: withWrapper(text => {
+      el.textContent = text;
+    }),
+    setClass: withWrapper((...classes) => {
+      el.classList.add(...classes);
+    }),
+    setAttribute: withWrapper((attributeName, value) => {
+      el.setAttribute(attributeName, value);
+    }),
+
+    // Getters
+    getInnerHtml: withWrapper(() => el.innerHTML),
+    getText: withWrapper(() => el.textContent),
+    getClasses: withWrapper(() => {
+      const classes = el.className.split(' ');
+
+      return classes.length === 0
+        ? []
+        : classes;
+    }),
+    getAttribute: withWrapper((attributeName) => el.getAttribute(attributeName)),
+    getAttributes: withWrapper(() => {
+      const attributesList = Array.prototype.slice.call(el.attributes);
+
+      return attributesList.reduce((acc, attribute) => ({
+        ...acc,
+        [attribute.name]: attribute.value
+      }), {});
     })
   };
 
